8b7a50efeac3072fd6d2a2b18117438c520ea61c
Jesse Posner <jesse@coinbase.com>
2018-10-22 22:18:10 -0700

    Add Numeration#exponentiation test

---
+++ spec/numeration_spec.rb
@@ -0,0 +1,9 @@
+require 'numeration'
+
+RSpec.describe Numeration do
+  describe '::exponentiation' do
+    it 'should calculate the correct result' do
+      expect(Numeration.exponentiation(4, 8)).to eq(4**8)
+    end
+  end
+end
