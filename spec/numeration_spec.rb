19e7f9f600e56df5cf8b881e584991e3bc221da3
Jesse Posner <jesse@coinbase.com>
2018-10-24 23:10:37 -0700

    Add tests

--- spec/numeration_spec.rb
+++ spec/numeration_spec.rb
@@ -3,9 +3,41 @@
 require 'numeration'
 
 RSpec.describe Numeration do
+  describe '::convert_int_to_string' do
+    it 'should calculate the correct result' do
+      described_class::SUPPORTED_RADIXES.each do |radix|
+        next if radix == 64
+
+        (0..100).each do |int|
+          expect(
+            described_class.convert_int_to_string(int: int,
+                                                  radix: radix)
+          ).to eq(int.to_s(radix))
+        end
+      end
+    end
+
+    context 'when the radix is not supported' do
+      it 'should raise an error' do
+        expect do
+          described_class.convert_int_to_string(
+            int: 1,
+            radix: described_class::SUPPORTED_RADIXES.max + 1
+          )
+        end.to raise_error(StandardError, 'radix not supported')
+      end
+    end
+  end
+
   describe '::exponentiation' do
     it 'should calculate the correct result' do
-      expect(Numeration.exponentiation(4, 8)).to eq(4**8)
+      (0..100).each do |base|
+        (100..0).each do |exponent|
+          expect(
+            described_class.exponentiation(base, exponent)
+          ).to eq(base**exponent)
+        end
+      end
     end
   end
 end
