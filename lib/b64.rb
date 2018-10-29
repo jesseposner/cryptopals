5a9611c2862871ec368e16b64d364ed61f696a10
Jesse Posner <jesse@coinbase.com>
2018-10-29 01:56:11 -0700

    Add ::bytes and ::bits to B64 and refactor

--- lib/b64.rb
+++ lib/b64.rb
@@ -5,18 +5,32 @@ require_relative 'byte'
 
 class B64
   # TODO: add B64 padding
-  def self.encode_ascii(str)
-    Byte::String
-      .new(str)
-      .bits
-      .chars
-      .each_slice(6)
-      .map do |sextet|
-        int = Numeration
-              .string_to_integer(string: sextet.join, radix: 2)
-        Numeration
-          .integer_to_string(int: int, radix: 64)
-      end
-      .join
+  class Encode
+    def self.bits(str)
+      str
+        .chars
+        .each_slice(6)
+        .map do |sextet|
+          int = Numeration
+                .string_to_integer(string: sextet.join, radix: 2)
+          Numeration
+            .integer_to_string(int: int, radix: 64)
+        end
+        .join
+    end
+
+    def self.bytes(arr)
+      bits = arr
+             .map { |byte| Byte::Integer.new(byte).bits }
+             .join
+
+      bits(bits)
+    end
+
+    def self.ascii(str)
+      bits = Byte::String.new(str).bits
+
+      bits(bits)
+    end
   end
 end
