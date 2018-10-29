26eb4f51de5053642cb34621c199fab353290ba2
Jesse Posner <jesse@coinbase.com>
2018-10-29 01:20:37 -0700

    Refactor B64

--- lib/b64.rb
+++ lib/b64.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
+
 require_relative 'numeration'
+require_relative 'byte'
 
 class B64
-  # TODO add B64 padding
-  def self.encode(str)
-    Numeration
-      .convert_str_to_bits(str)
-      .join
-      .split('')
+  # TODO: add B64 padding
+  def self.encode_ascii(str)
+    Byte::String
+      .new(str)
+      .bits
+      .chars
       .each_slice(6)
-      .map(&:join)
       .map do |sextet|
         int = Numeration
-          .string_to_integer(string: sextet, radix: 2)
+              .string_to_integer(string: sextet.join, radix: 2)
         Numeration
           .integer_to_string(int: int, radix: 64)
-       end
+      end
       .join
   end
 end
