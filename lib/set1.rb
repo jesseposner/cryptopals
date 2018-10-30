2fab9d7ca752f600ca99cb175affc3609d9bbda3
Jesse Posner <jesse@coinbase.com>
2018-10-30 00:26:09 -0700

    Add Set1 challenge 3 helper methods

--- lib/set1.rb
+++ lib/set1.rb
@@ -4,6 +4,7 @@ require_relative 'hex'
 require_relative 'b64'
 require_relative 'xor'
 require_relative 'numeration'
+require_relative 'alphabet'
 
 class Set1
   def self.hex_string_to_base64(hex)
@@ -20,5 +21,27 @@ class Set1
       xor_int = XOR.int(hex1_byte, hex2_bytes[idx])
       Numeration.integer_to_string(int: xor_int, radix: 16)
     end.join
+  end
+
+  def self.xor_combine_hex_and_single_char(hex, char)
+    hex_bytes = Hex.new(hex).bytes
+    char_byte = char.ord
+    xor_hex   = hex_bytes.map do |byte|
+      xor_int = XOR.int(byte, char_byte)
+      Numeration.integer_to_string(int: xor_int, radix: 16)
+    end.join
+
+    Hex.new(xor_hex).ascii
+  end
+
+  def self.best_score(string_arr)
+    string_arr.max_by { |str| score_string(str) }
+  end
+
+  def self.score_string(str)
+    str
+      .chars
+      .map { |char| Alphabet::LETTER_FREQUENCY_SCORES[char.to_sym.upcase] || 0 }
+      .sum
   end
 end
