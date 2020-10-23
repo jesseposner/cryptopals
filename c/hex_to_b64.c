832feaccf4ae77c4027f250821ac78270766a07a
Jesse Posner <jesse.posner@gmail.com>
2020-10-23 00:14:13 -0700

    Add base64 encoding

--- c/hex_to_b64.c
+++ c/hex_to_b64.c
@@ -4,10 +4,12 @@
 
 char* decode_hex(const char*);
 unsigned char decode_hex_char(const unsigned char);
+char* encode_b64(const char*);
 
 
 int main(int argc, char *argv[]) {
     printf("%s\n", decode_hex(argv[1]));
+    printf("%s\n", encode_b64(decode_hex(argv[1])));
 }
 
 char* decode_hex(const char *hex) {
@@ -45,4 +47,27 @@ unsigned char decode_hex_char(const unsigned char h) {
     }
 
     return decoded_value;
+}
+
+char* encode_b64(const char *bytes) {
+    /* TODO: add padding */
+    size_t len, i, bits;
+    const char *b64_symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    len = strlen(bytes);
+    len /= 3;
+    char *out = malloc(len*4);
+    memset(out, 'A', len);
+
+    for (i = 0; i < len; ++i) {
+        bits = bytes[i*3] << 8 | bytes[i*3+1];
+        bits = bits << 8 | bytes[i*3+2];
+
+        out[i*4] = b64_symbols[bits >> 18];
+        out[i*4+1] = b64_symbols[(bits >> 12) & 63];
+        out[i*4+2] = b64_symbols[(bits >> 6) & 63];
+        out[i*4+3] = b64_symbols[bits & 63];
+    }
+
+    return out;
 }
